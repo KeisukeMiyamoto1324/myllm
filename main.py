dffa3272e4a4184a8de77c0975faf6af22491a73
KeisukeMiyamoto1324 <129528528+KeisukeMiyamoto1324@users.noreply.github.com>
2026-04-04 10:47:50 -0500

    feat: Add token mapping and input tensor

--- main.py
+++ main.py
@@ -8,4 +8,34 @@ from torch.utils.data import TensorDataset, DataLoader
 import lightning as L
 
 
+token_to_id = {
+    "what": 0,
+    "is": 1,
+    "statquest": 2,
+    "awesome": 3,
+    "<EOS>": 4
+}
+# {0: 'what', 1: 'is', 2: 'statquest', 3: 'awesome', 4: '<EOS>'}
+id_to_token = dict(map(reversed, token_to_id.items()))  
+
+# tensor([[0, 1, 2, 4, 3],
+#         [2, 1, 0, 4, 3]])
+inputs = torch.tensor(
+    [
+        [
+            token_to_id["what"],
+            token_to_id["is"],
+            token_to_id["statquest"],
+            token_to_id["<EOS>"],
+            token_to_id["awesome"],
+        ],
+        [
+            token_to_id["statquest"],
+            token_to_id["is"],
+            token_to_id["what"],
+            token_to_id["<EOS>"],
+            token_to_id["awesome"],
+        ],
+    ]
+)
 
