16d3c30ce45ffb55c251581dc6b9af311955146a
KeisukeMiyamoto1324 <129528528+KeisukeMiyamoto1324@users.noreply.github.com>
2026-04-04 17:10:18 -0500

    feat: Add model initialization and trainer configuration

--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 from torch.utils.data import DataLoader
+import lightning as L
 
 from transformer import DecoderOnlyTransformer
-from tokenizer import tokenizer
+from tokenizer import tokenizer, vocabulary
 from dataset import get_dataset
 
 
@@ -9,7 +10,13 @@ sentences = [
     "what is statquest <EOS> awesome",
     "statquest is what <EOS> awesome",
 ]
+max_len = 6
 
 dataset = get_dataset(sentences=sentences, tokenizer=tokenizer)
 dataloader = DataLoader(dataset)
+
+model = DecoderOnlyTransformer(num_tokens=len(vocabulary), d_model=2, max_len=max_len)
+
+trainer = L.Trainer(max_epochs=30)
+trainer.fit(model, train_dataloaders=dataloader)
 
