20d1393c444949d06046b8944e2d6e40728ddef8
KeisukeMiyamoto1324 <129528528+KeisukeMiyamoto1324@users.noreply.github.com>
2026-04-04 11:17:07 -0500

    chore: Remove unused imports from position_encoding.py

--- position_encoding.py
+++ position_encoding.py
@@ -1,11 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
-from torch.optim import Adam
-from torch.utils.data import TensorDataset, DataLoader
-
-import lightning as L
 
 
 class PositionEncoding(nn.Module):
