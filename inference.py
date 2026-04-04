7590f44bea02f6152935d47c16fe6fc31903386c
KeisukeMiyamoto1324 <129528528+KeisukeMiyamoto1324@users.noreply.github.com>
2026-04-04 17:07:37 -0500

    feat: Add iterative inference loop and detokenizer output

--- inference.py
+++ inference.py
@@ -1,16 +1,29 @@
 import torch
 
 from transformer import DecoderOnlyTransformer
-from tokenizer import tokenizer, vocabulary
+from tokenizer import tokenizer, vocabulary, detokenizer
 
 
 prompt = "what is statquest <EOS> awesome"
+max_len = 6
 
-model = DecoderOnlyTransformer(num_tokens=len(vocabulary), d_model=2, max_len=6)
+model = DecoderOnlyTransformer(num_tokens=len(vocabulary), d_model=2, max_len=max_len)
 model_input = tokenizer(words=prompt.split())
 input_length = model_input.size(dim=0)
 
 predictions = model(model_input)
+predicted_id = torch.tensor([torch.argmax(predictions[-1, :])])
+predicted_ids = predicted_id
 
-print(predictions)
-# predicted_id = torch.tensor([torch.argmax(predictions[-1, :])])
+for i in range(input_length, max_len):
+    if predicted_id == tokenizer(["<EOS>"]):
+        break
+    
+    model_input = torch.cat((model_input, predicted_id))
+    
+    predictions = model(model_input)
+    predicted_id = torch.tensor([torch.argmax(predictions[-1, :])])
+    predicted_ids = torch.cat((predicted_ids, predicted_id))
+
+print(f"predicted tokens: {detokenizer(tokens=predicted_ids)}")
+
