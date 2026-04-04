52a7c398d2b6d9533585473be150fd76e6f9498c
KeisukeMiyamoto1324 <129528528+KeisukeMiyamoto1324@users.noreply.github.com>
2026-04-04 16:53:16 -0500

    feat: Add model inference and prediction output

--- inference.py
+++ inference.py
@@ -1,3 +1,5 @@
+import torch
+
 from transformer import DecoderOnlyTransformer
 from tokenizer import tokenizer, vocabulary
 
@@ -8,3 +10,7 @@ model = DecoderOnlyTransformer(num_tokens=len(vocabulary), d_model=2, max_len=6)
 model_input = tokenizer(words=prompt.split())
 input_length = model_input.size(dim=0)
 
+predictions = model(model_input)
+
+print(predictions)
+# predicted_id = torch.tensor([torch.argmax(predictions[-1, :])])
