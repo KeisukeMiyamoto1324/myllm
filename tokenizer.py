3d223925da2057a305f1ffb8c7c407cb08747206
KeisukeMiyamoto1324 <129528528+KeisukeMiyamoto1324@users.noreply.github.com>
2026-04-09 11:35:54 -0500

    refactor: Convert tokenizer functions to a class structure

--- tokenizer.py
+++ tokenizer.py
@@ -1,11 +1,19 @@
 import torch
 
 
-vocabulary = ["what", "is", "statquest", "awesome", "<EOS>"]
-token_map = {w: i for i, w in enumerate(vocabulary)}
+class Tokenizer:
+    def __init__(self):
+        self.vocabulary: set[str] = set()
+        self.token_map: dict[str, int] = {}
+        self.id_to_token: list[str] = []
+    
+    def learn_vocab(self, sentences: list[str]) -> None:
+        self.vocabulary = {word for sentence in sentences for word in sentence.split()}
+        self.id_to_token = sorted(self.vocabulary)
+        self.token_map = {word: i for i, word in enumerate(self.id_to_token)}
 
-def tokenizer(words: str) -> torch.Tensor:
-    return torch.tensor([token_map[w] for w in words.split()])
+    def tokenizer(self, words: str) -> torch.Tensor:
+        return torch.tensor([self.token_map[w] for w in words.split()])
 
-def detokenizer(tokens: torch.Tensor) -> list[str]:
-    return [vocabulary[token.item()] for token in tokens]
+    def detokenizer(self, tokens: torch.Tensor) -> list[str]:
+        return [self.id_to_token[token.item()] for token in tokens]
