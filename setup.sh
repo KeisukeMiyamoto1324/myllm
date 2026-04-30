53e70e683d9c7a10124ad1064cd8555dfd1c39bb
KeisukeMiyamoto1324 <aichiboyhighschool@gmail.com>
2026-04-30 00:57:13 +0000

    chore: Update requirements file in setup script to use requirements.txt

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ sudo apt install python3.10-venv -y
 python3 -m venv venv
 source venv/bin/activate
 pip3 install --upgrade pip
-pip3 install -r requirements-arm.txt
+pip3 install -r requirements.txt
 
 git config --local user.name "KeisukeMiyamoto1324"
 git config --local user.email "aichiboyhighschool@gmail.com"
