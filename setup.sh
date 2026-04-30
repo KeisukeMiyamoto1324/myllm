ad5e417ff304ece54c51a3fb81c6f0bbad33ad79
KeisukeMiyamoto1324 <129528528+KeisukeMiyamoto1324@users.noreply.github.com>
2026-04-29 19:44:19 -0500

    chore: Add git config, nvm, and npm global packages to setup script

--- setup.sh
+++ setup.sh
@@ -4,3 +4,13 @@ python3 -m venv venv
 source venv/bin/activate
 pip3 install --upgrade pip
 pip3 install -r requirements-arm.txt
+
+git config --local user.name "KeisukeMiyamoto1324"
+git config --local user.email "aichiboyhighschool@gmail.com"
+
+curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+nvm install 22
+nvm use 22
+npm i -g @openai/codex
